25f67fd2265feaf89e5bd27fe496406194f81cba
Caio Yoshio Itiroko <caioitiroko@ymail.com>
2018-06-18 21:42:16 -0300

    Add validates

--- app/models/grow_bed.rb
+++ app/models/grow_bed.rb
@@ -1,2 +1,3 @@
 class GrowBed < ApplicationRecord
+  validates :sample_type, :n_sample, presence: true
 end
--- app/models/grow_bed_datum.rb
+++ app/models/grow_bed_datum.rb
@@ -1,2 +1,3 @@
 class GrowBedDatum < ApplicationRecord
+  validates :date, :avg_width, :avg_length, :temperature, :water_flow, :lux, :ph, :n_fish, presence: true
 end
