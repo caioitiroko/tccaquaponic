a91136f45fb1b9ec90876169136ea9d56bacc7d8
Caio Yoshio Itiroko <caioitiroko@ymail.com>
2018-06-19 20:12:51 -0300

    Update header menu

--- app/admin/grow_bed_data.rb
+++ app/admin/grow_bed_data.rb
@@ -1,3 +1,5 @@
 ActiveAdmin.register GrowBedDatum do
+  menu priority: 3
+
   permit_params :date, :avg_width, :avg_length, :temperature, :water_flow, :lux, :ph, :n_fish, :grow_bed_id
 end
--- app/admin/grow_beds.rb
+++ app/admin/grow_beds.rb
@@ -1,3 +1,5 @@
 ActiveAdmin.register GrowBed do
+  menu priority: 2
+
   permit_params :sample_type, :n_sample, :active
 end
