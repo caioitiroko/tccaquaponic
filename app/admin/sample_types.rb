6cfd25adecacb713722aab6db1a103b3a9233a38
Caio Yoshio Itiroko <caioitiroko@ymail.com>
2018-06-23 12:23:18 -0300

    Upgrade SampleType layout

--- app/admin/sample_types.rb
+++ app/admin/sample_types.rb
@@ -1,15 +1,34 @@
 ActiveAdmin.register SampleType do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  menu priority: 4
 
+  permit_params :name
+
+  config.batch_actions = false
+  config.sort_order = :name_asc
+
+  filter :name
+
+  index do
+    column :name
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+    end
+  end
+
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+
+    f.inputs do
+      f.semantic_errors :base
+
+      f.input :name
+    end
+
+    f.actions
+  end
 end
