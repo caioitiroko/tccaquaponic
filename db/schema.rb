f3f075a6fcfa9992d859526092a5ef9e86d2a959
Caio Yoshio Itiroko <caioitiroko@ymail.com>
2018-06-23 12:09:54 -0300

    Add SampleType

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_23_135510) do
+ActiveRecord::Schema.define(version: 2018_06_23_144602) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -66,6 +66,12 @@ ActiveRecord::Schema.define(version: 2018_06_23_135510) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.index ["name"], name: "index_grow_beds_on_name", unique: true
+  end
+
+  create_table "sample_types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
