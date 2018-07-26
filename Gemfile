2afcb5955470b37b4947a28a12729fce42b09ce1
Caio Yoshio Itiroko <caioitiroko@ymail.com>
2018-07-26 20:04:37 -0300

    Add a method to get how much a datum growth

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem 'draper'
 gem 'pundit'
 
 gem 'active_skin'
+gem 'memoist'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/grow_bed_datum.rb
+++ app/models/grow_bed_datum.rb
@@ -1,5 +1,23 @@
+require 'memoist'
+
 class GrowBedDatum < ApplicationRecord
+  extend Memoist
+
   belongs_to :grow_bed
 
   validates :date, :avg_width, :avg_length, :temperature, :water_flow, :lux, :ph, :n_fish, presence: true
+
+  def growth_width
+    avg_width - previous_datum.avg_width if previous_datum
+  end
+
+  def growth_height
+    avg_height - previous_datum.avg_height if previous_datum
+  end
+
+  def previous_datum
+    GrowBedDatum.where("date < :date and grow_bed_id = :grow_bed", { date: date, grow_bed: grow_bed  }).order(:date).last
+  end
+
+  memoize :growth_height, :growth_width, :previous_datum
 end
